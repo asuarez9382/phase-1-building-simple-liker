13c1432f77e6732dc6e16e47933c059cacf506f2
Adrian Suarez <asuarez9382@gmail.com>
2023-11-19 12:18:13 -0800

    complete lab

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heart = document.querySelector(".like-glyph")
 
+//Adds an event listener on click to heart
+heart.addEventListener("click", ()=>{
+  mimicServerCall()
+  .then(res => {
+    const heartProperty = heart.innerHTML
+    if(heartProperty === FULL_HEART) {
+      heart.innerHTML = EMPTY_HEART
+      heart.className = ""
+    }
+    else {
+      heart.innerHTML = FULL_HEART;
+      heart.className = "activated-heart";
+    }
+  })
+  .catch(error => {
+    const modal = document.querySelector("#modal");
+    modal.className = "";
+    const p = document.querySelector("#modal-message");
+    p.innerHTML = error;
+    setTimeout(errorMessage,3000)
+  })
+})
 
+function errorMessage() {
+  modal.className = "hidden";
+}
 
 
 //------------------------------------------------------------------------------
